9d3fb12eda388f3ff8e845d05aee61340d9a9639
omkava <omkava9@gmail.com>
2026-02-18 16:11:19 +0530

    Update BASE_URL to new Vercel deployment

--- client/services/apis.js
+++ client/services/apis.js
@@ -1,4 +1,4 @@
-const BASE_URL = "https://skillstream-edtech.onrender.com";
+const BASE_URL = "https://skillstream-edtech-e9sb.vercel.app";
 
 export const catagories = {
     CATAGORIES_API: `${BASE_URL}/api/v1/course/showallcatagories`,
